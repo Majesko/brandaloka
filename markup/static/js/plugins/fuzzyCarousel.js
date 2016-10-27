c0acc27e737fd3900e991144a2579837a537bef6
Majesko <majesko@gmail.com>
2016-10-27 10:24:07 +0300

    fixes

--- markup/static/js/plugins/fuzzyCarousel.js
+++ markup/static/js/plugins/fuzzyCarousel.js
@@ -1,23 +1,29 @@
-let fuzzyCarousel = (selector, options) => {
+window.fuzzyCarousel = function(selector, options) {
     /*
     options: selector, width, count
     */
-    let width = options.width;
-    let count = options.count;
-    let position = 0;
-    let carousel = document.getElementById(selector);
-    let list = carousel.querySelector('.carousel__list');
-    let listElems = carousel.querySelectorAll('.carousel__item');
-    let controlBox = carousel.querySelector('.carousel__controls');
-    let dotBox = document.createElement('ul');
-    let itemPosition = position;
+    var width = options.width;
+    var count = options.count;
+    var position = 0;
+    var carousel = document.getElementById(selector);
+    var list = carousel.querySelector('.carousel__list');
+    var listElems = carousel.querySelectorAll('.carousel__item');
+    var itemPosition = position;
     carousel.style.width = options.width+'px';
     
-    addDotBox = () => {
+    var controlBox = document.createElement('div');
+    controlBox.className = 'carousel__controls';
+    controlBox.innerHTML = 
+        '<a href="#" class="carousel__button carousel__left"></a>'+
+        '<a href="#" class="carousel__button carousel__right"></a>';
+    carousel.appendChild(controlBox);
+
+    addDotBox = function() {
+        var dotBox = document.createElement('ul');
         dotBox.className = 'carousel__dotbox';
         controlBox.appendChild(dotBox);
-        for (let i = 0, len = listElems.length; i < len; i++) {
-            let item = document.createElement('li');
+        for (var i = 0, len = listElems.length; i < len; i++) {
+            var item = document.createElement('li');
             item.className = 'carousel__dot';
             item.setAttribute('data-position', -itemPosition);
             itemPosition = itemPosition + width;
@@ -27,12 +33,13 @@ let fuzzyCarousel = (selector, options) => {
             };
         }
     };
-    // addDotBox();
+
+
     if(options.dots) {
         addDotBox();
     }
     carousel.querySelector('.carousel__left').onclick = function () {
-        let minPosition = Math.min(position + width * count, 0);
+        var minPosition = Math.min(position + width * count, 0);
         if (position === minPosition) {
             minPosition = -(width * (listElems.length - count));
         }
@@ -41,7 +48,7 @@ let fuzzyCarousel = (selector, options) => {
         return false;
     };
     carousel.querySelector('.carousel__right').onclick = function () {
-        let maxPosition = Math.max(position - width * count, -width * (listElems.length - count));
+        var maxPosition = Math.max(position - width * count, -width * (listElems.length - count));
         if (position === maxPosition) {
             maxPosition = 0;
         }
